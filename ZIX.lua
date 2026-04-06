7e2ca8d62d680a8f9a392c8ae9a75a0ebd316f8d
ZIX_HUB <sxazyhub@gmail.com>
2026-04-05 17:44:55 -0700

    Update ZIX.lua

--- ZIX.lua
+++ ZIX.lua
@@ -21,7 +21,7 @@ local displayName = player.DisplayName or player.Name
 
 local window = library:AddWindow("ZIX HUB - PUBLIC" .. displayName, {
     main_color = Color3.fromRGB(75, 0, 130),
-    min_size = Vector2.new(420, 340),
+    min_size = Vector2.new(420, 870),
     can_resize = false,
 })
 
