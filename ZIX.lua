0a14d6c6d2339725eadeab6feb9e7d65f64fad23
ZIX_HUB <sxazyhub@gmail.com>
2026-04-05 19:17:37 -0700

    Update ZIX.lua

--- ZIX.lua
+++ ZIX.lua
@@ -16,7 +16,7 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local title = ("ZIX Hub | Hello -%s"):format(displayName)
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/UserTy12008/Ty/refs/heads/main/Lybrery"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/UserTy12008/Ty/refs/heads/main/Lybrery",true))()
 
 local window = library:AddWindow(title, {
     main_color = Color3.fromRGB(128, 0, 255),
